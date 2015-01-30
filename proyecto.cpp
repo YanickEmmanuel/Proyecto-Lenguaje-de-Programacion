7f82636f0bf8cab6d4a409cc2740e6d769dccf3a
laptrock <alejandro_lapt@hotmail.com>
2015-01-29 20:45:08 -0500

    Revert "Revert "alejandro""
    This reverts commit c11775ca0a2ad9edc83cdb4ec90872f1ec142ccd.

--- proyecto.cpp
+++ proyecto.cpp
@@ -16,3 +16,45 @@ int menu(){
    return op;
 }
 
+//Función que genera la tabla de verdad AND
+
+void generarAnd(int lineas,int nEntradas){
+   ofstream fs("nombre2.txt");
+   ifstream fe("nombre.txt");
+   char cadena[128];
+
+   //Copia el contenido de nombre.txt a nombre2.txt
+   while(!fe.eof()) {
+      fe.getline(cadena, 128);
+      fs<<cadena<<endl;
+   }
+
+	//Genera el encabezado
+   for(int i=1;i<=nEntradas;i++){
+   	fs<<"X"<<i<<" ";
+	   if(i==nEntradas)
+     	fs<<"AND"<<endl;
+	}
+	//Genera la tabla de verdad AND
+   for(int i=1;i<=lineas;i++){
+   	bool and=1;
+      for(int j=nEntradas;j>=1;j--){
+	     	int divisor=pow(2,j);
+     	   int digito;
+        	digito=(i%divisor);
+
+	      if(digito<=(divisor/2)){
+            if(digito==0)
+     	      	digito=1;
+        	   else
+        			digito=0;
+	      }
+         else
+     	   	digito=1;
+
+	      if(digito==0)
+         	and=0;
+     	   fs<<digito<<"  ";
+	   }
+      fs<<and<<endl;
+   }
